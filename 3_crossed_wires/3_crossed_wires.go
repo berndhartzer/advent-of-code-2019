d07c7560f4d9e3f4922defac6f3ee50202892788
Bernd Hartzer <berndhartzer@gmail.com>
2019-12-03 21:06:31 +1000

    day 3 incomplete

--- 3_crossed_wires/3_crossed_wires.go
+++ 3_crossed_wires/3_crossed_wires.go
@@ -1,5 +1,89 @@
 package three_crossed_wires
 
+import (
+	"fmt"
+	"strings"
+	"strconv"
+)
+
+type Coord struct {
+	X, Y int
+}
+
+func (c *Coord) Move(direction string) {
+	switch direction {
+	case "U":
+		c.Y += 1
+	case "D":
+		c.Y -= 1
+	case "R":
+		c.X += 1
+	case "L":
+		c.X -= 1
+	}
+}
+
 func CrossedWiresPartOne(wires [][]string) int {
-	return 0
+	grid := make(map[string][]int)
+
+	for i, wire := range wires {
+
+		coord := Coord{0, 0}
+		grid[fmt.Sprintf("%d,%d", coord.X, coord.Y)] = append(grid[fmt.Sprintf("%d,%d", coord.X, coord.Y)], i)
+
+		for _, instruction := range wire {
+
+			split := []rune(instruction)
+			distance, _ := strconv.Atoi(string(split[1:]))
+			// fmt.Println(split, distance)
+
+			for j := 0; j < distance; j++ {
+				// coord.Move(split[0])
+				coord.Move(string(split[0]))
+				grid[fmt.Sprintf("%d,%d", coord.X, coord.Y)] = append(grid[fmt.Sprintf("%d,%d", coord.X, coord.Y)], i)
+			}
+
+		}
+	}
+
+	overlaps := []string{}
+	for coord, hits := range grid {
+		if len(hits) > 1 {
+			if coord != "0,0" {
+				uniqueWires := make(map[int]bool)
+				for _, hit := range hits {
+					uniqueWires[hit] = true
+				}
+
+				if len(uniqueWires) > 1 {
+					overlaps = append(overlaps, coord)
+				}
+			}
+		}
+	}
+	// fmt.Println(overlaps)
+
+	closest := 0
+	for _, coord := range overlaps {
+		// fmt.Println(coord)
+		split := strings.Split(coord, ",")
+		x, _ := strconv.Atoi(split[0])
+		y, _ := strconv.Atoi(split[1])
+
+		distance := Abs(x) + Abs(y)
+		// fmt.Println(distance)
+		if distance < closest || closest == 0 {
+			closest = x + y
+		}
+	}
+
+
+	return closest
+}
+
+func Abs(n int) int {
+	if n < 0 {
+		return -n
+	}
+	return n
 }
--- 3_crossed_wires/3_crossed_wires_test.go
+++ 3_crossed_wires/3_crossed_wires_test.go
@@ -1,35 +1,65 @@
 package three_crossed_wires
 
 import (
+	"bufio"
+	"log"
+	"os"
 	"testing"
+	"strings"
 )
 
 func TestCrossedWiresPartOneExampleInput(t *testing.T) {
-	// R8,U5,L5,D3
-	// U7,R6,D4,L4
-	// 6
 	exampleInputOne := [][]string{
 		{"R8", "U5", "L5", "D3"},
 		{"U7", "R6", "D4", "L4"},
 	}
 
-	// R75,D30,R83,U83,L12,D49,R71,U7,L72
-	// U62,R66,U55,R34,D71,R55,D58,R83
-	// 159
+	exampleInputTwo := [][]string{
+		{"R75", "D30", "R83", "U83", "L12", "D49", "R71", "U7", "L72"},
+		{"U62", "R66", "U55", "R34", "D71", "R55", "D58", "R83"},
+	}
 
-	// R98,U47,R26,D63,R33,U87,L62,D20,R33,U53,R51
-	// U98,R91,D20,R16,D67,R40,U7,R15,U6,R7
-	// 135
+	exampleInputThree := [][]string{
+		{"R98", "U47", "R26", "D63", "R33", "U87", "L62", "D20", "R33", "U53", "R51"},
+		{"U98", "R91", "D20", "R16", "D67", "R40", "U7", "R15", "U6", "R7"},
+	}
 
 	expectedOutputOne := 6
+	expectedOutputTwo := 159
+	expectedOutputThree := 135
 
 	exampleOutputOne := CrossedWiresPartOne(exampleInputOne)
+	exampleOutputTwo := CrossedWiresPartOne(exampleInputTwo)
+	exampleOutputThree := CrossedWiresPartOne(exampleInputThree)
 
 	if exampleOutputOne != expectedOutputOne {
+		t.Fail()
+	}
+
+	if exampleOutputTwo != expectedOutputTwo {
+		t.Fail()
+	}
+
+	if exampleOutputThree != expectedOutputThree {
 		t.Fail()
 	}
 }
 
 func TestCrossedWiresPartOneActualInput(t *testing.T) {
+	t.Skip()
+	file, err := os.Open("./3_input.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
 
+	var input [][]string
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		line := scanner.Text()
+		input = append(input, strings.Split(line, ","))
+	}
+
+	output := CrossedWiresPartOne(input)
+	t.Log(output)
 }
