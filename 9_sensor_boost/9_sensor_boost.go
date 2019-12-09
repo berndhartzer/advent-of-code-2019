acfe45f5e5ef7b4bcdd2fdb450c1b67a92d381ce
Bernd Hartzer <berndhartzer@gmail.com>
2019-12-09 21:15:47 +1000

    day 9 incomplete

--- 9_sensor_boost/9_sensor_boost.go
+++ 9_sensor_boost/9_sensor_boost.go
@@ -1,5 +1,169 @@
 package nine_sensor_boost
 
-func SensorBoostPartOne() int {
-	return 0
+import (
+	"fmt"
+)
+
+func SensorBoostPartOne(program []int, input int) []int {
+	// output, _, _ := IntCodeComputer(program, &dst, [2]int{0, 0}, 0, false)
+	output := []int{}
+	IntCodeComputer(program, &output, [2]int{input, input}, 0, false)
+	return output
+}
+
+func IntCodeComputer(originalProgram []int, dst *[]int, input [2]int, i int, returnOutput bool) {
+	program := make([]int, 5000)
+	copy(program, originalProgram)
+
+	inputIndex := 0
+	relativeBase := 0
+	for {
+		if program[i] == 99 {
+			if returnOutput {
+				// return *(dst)[len(*dst)-1], i, true
+				return
+			}
+			break
+		}
+
+		opCodeRaw := program[i]
+		opCodeParts := [5]int{}
+		for j := 4; j >= 0; j -= 1 {
+			opCodeParts[j] = opCodeRaw % 10
+			opCodeRaw = opCodeRaw / 10
+		}
+		fmt.Println(opCodeParts)
+
+		opCode := opCodeParts[4]
+		paramOneMode := opCodeParts[2]
+		paramTwoMode := opCodeParts[1]
+		paramThreeMode := opCodeParts[0]
+
+		paramOne := 0
+		if paramOneMode == 0 {
+			paramOne = program[program[i+1]]
+		} else if paramOneMode == 1 {
+			paramOne = program[i+1]
+		} else if paramOneMode == 2 {
+			paramOne = program[relativeBase + program[i+1]]
+		}
+
+		switch opCode {
+		case 0:
+			return // TODO: remove
+		case 1, 2:
+			paramTwo := 0
+			if paramTwoMode == 0 {
+				paramTwo = program[program[i+2]]
+			} else if paramTwoMode == 1 {
+				paramTwo = program[i+2]
+			} else if paramTwoMode == 2 {
+				paramTwo = program[relativeBase + program[i+2]]
+			}
+
+			// outputPos := program[i+3]
+			paramThree := 0
+			if paramThreeMode == 0 {
+				paramThree = program[program[i+3]]
+			} else if paramThreeMode == 1 {
+				// 
+			} else if paramThreeMode == 2 {
+				paramThree = program[relativeBase + program[i+3]]
+			}
+
+			if opCode == 1 {
+				// program[outputPos] = paramOne + paramTwo
+				program[paramThree] = paramOne + paramTwo
+			} else {
+				// program[outputPos] = paramOne * paramTwo
+				program[paramThree] = paramOne * paramTwo
+			}
+			i += 4
+
+		case 3:
+			// posOne := program[i+1]
+			// if paramOneMode == 0 {
+			// 	program[posOne] = input[inputIndex]
+			// } else if paramOneMode == 2 {
+			// 	// program[posOne] = input[inputIndex]
+			// 	program[relativeBase + program[paramOne]] = input[inputIndex]
+			// }
+
+
+			// paramOne := 0
+			if paramOneMode == 0 {
+				program[program[i+1]] = input[inputIndex]
+			} else if paramOneMode == 1 {
+				// paramOne = program[i+1]
+			} else if paramOneMode == 2 {
+				program[relativeBase + program[i+1]] = input[inputIndex]
+			}
+
+
+			inputIndex += 1
+			i += 2
+
+		case 4:
+			// program = append(program, paramOne)
+			*dst = append(*dst, paramOne)
+			i += 2
+			if returnOutput {
+				// return program[len(program)-1], i, false
+				return
+			}
+
+		case 5, 6:
+			paramTwo := 0
+			if paramTwoMode == 0 {
+				paramTwo = program[program[i+2]]
+			} else if paramTwoMode == 1 {
+				paramTwo = program[i+2]
+			} else if paramTwoMode == 2 {
+				paramTwo = program[relativeBase + program[i+2]]
+			}
+
+			if (opCode == 5 && paramOne != 0) || (opCode == 6 && paramOne == 0) {
+				i = paramTwo
+			} else {
+				i += 3
+			}
+
+		case 7, 8:
+			paramTwo := 0
+			if paramTwoMode == 0 {
+				paramTwo = program[program[i+2]]
+			} else if paramTwoMode == 1 {
+				paramTwo = program[i+2]
+			} else if paramTwoMode == 2 {
+				paramTwo = program[relativeBase + program[i+2]]
+			}
+
+			// outputPos := program[i+3]
+			paramThree := 0
+			if paramThreeMode == 0 {
+				paramThree = program[program[i+3]]
+			} else if paramThreeMode == 1 {
+				// 
+			} else if paramThreeMode == 2 {
+				paramThree = program[relativeBase + program[i+3]]
+			}
+
+			if (opCode == 7 && paramOne < paramTwo) || (opCode == 8 && paramOne == paramTwo) {
+				// program[outputPos] = 1
+				program[paramThree] = 1
+			} else {
+				// program[outputPos] = 0
+				program[paramThree] = 0
+			}
+			i += 4
+
+		case 9:
+			relativeBase += paramOne
+			i += 2
+
+		}
+	}
+
+	// return dst[len(dst)-1], i, false
+	return
 }
--- 9_sensor_boost/9_sensor_boost_test.go
+++ 9_sensor_boost/9_sensor_boost_test.go
@@ -1,9 +1,57 @@
 package nine_sensor_boost
 
 import (
+	"bufio"
+	"os"
+	"strconv"
+	"strings"
 	"testing"
 )
 
-func SensorBoostPartOneExampleInput(t *testing.T) {
+func TestSensorBoostPartOneExampleInput(t *testing.T) {
+	exampleOneInput := []int{109, 1, 204, -1, 1001, 100, 1, 100, 1008, 100, 16, 101, 1006, 101, 0, 99}
+	exampleOneOutput := SensorBoostPartOne(exampleOneInput, 0)
 
+	for i := 0; i < len(exampleOneInput); i += 1 {
+		if exampleOneInput[i] != exampleOneOutput[i] {
+			t.Fail()
+		}
+	}
+
+	exampleInputs := [][]int{
+		{1102, 34915192, 34915192, 7, 4, 7, 99, 0},
+		{104, 1125899906842624, 99},
+	}
+	expectedExampleOutputs := []int{
+		1219070632396864,
+		1125899906842624,
+	}
+
+	for i := range exampleInputs {
+		output := SensorBoostPartOne(exampleInputs[i], 0)
+
+		if output[len(output)-1] != expectedExampleOutputs[i] {
+			t.Fail()
+		}
+	}
+}
+
+func TestSensorBoostPartOneActualInput(t *testing.T) {
+	var inputSplit []string
+	file, _ := os.Open("./9_input.txt")
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		line := scanner.Text()
+		inputSplit = strings.Split(line, ",")
+	}
+
+	input := []int{}
+
+	for _, str := range inputSplit {
+		num, _ := strconv.Atoi(str)
+		input = append(input, num)
+	}
+
+	output := SensorBoostPartOne(input, 1)
+	t.Log(output)
 }
