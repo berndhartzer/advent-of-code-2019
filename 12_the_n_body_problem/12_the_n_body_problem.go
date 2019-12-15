ad0d4088f891d05681124317194b0f5f23ad21f8
Bernd Hartzer <berndhartzer@gmail.com>
2019-12-15 20:17:38 +1000

    day 12 part 1

--- 12_the_n_body_problem/12_the_n_body_problem.go
+++ 12_the_n_body_problem/12_the_n_body_problem.go
@@ -1,5 +1,95 @@
 package twelve_the_n_body_problem
 
-func TheNBodyProblemPartOne(moonPositions []string) int{
-	return 0
+import (
+	"regexp"
+	"strconv"
+)
+
+type Axis struct {
+	X, Y, Z int
+}
+
+type Moon struct {
+	Position, Velocity Axis
+}
+
+func (m *Moon) applyGravity(moon *Moon) {
+	if m.Position.X > moon.Position.X {
+		m.Velocity.X -= 1
+		moon.Velocity.X += 1
+	} else if m.Position.X < moon.Position.X {
+		m.Velocity.X += 1
+		moon.Velocity.X -= 1
+	}
+
+	if m.Position.Y > moon.Position.Y {
+		m.Velocity.Y -= 1
+		moon.Velocity.Y += 1
+	} else if m.Position.Y < moon.Position.Y {
+		m.Velocity.Y += 1
+		moon.Velocity.Y -= 1
+	}
+
+	if m.Position.Z > moon.Position.Z {
+		m.Velocity.Z -= 1
+		moon.Velocity.Z += 1
+	} else if m.Position.Z < moon.Position.Z {
+		m.Velocity.Z += 1
+		moon.Velocity.Z -= 1
+	}
+}
+
+func (m *Moon) applyVelocity() {
+	m.Position.X += m.Velocity.X
+	m.Position.Y += m.Velocity.Y
+	m.Position.Z += m.Velocity.Z
+}
+
+func (m *Moon) getTotalEnergy() int {
+	potentialEnergy := Abs(m.Position.X) + Abs(m.Position.Y) + Abs(m.Position.Z)
+	kineticEnergy := Abs(m.Velocity.X) + Abs(m.Velocity.Y) + Abs(m.Velocity.Z)
+	return potentialEnergy * kineticEnergy
+}
+
+func TheNBodyProblemPartOne(moonPositions []string, simulations int) int {
+	moons := []*Moon{}
+
+	re := regexp.MustCompile(`[-]?\d[\d]*[\.]?[\d{2}]*`)
+	for _, position := range moonPositions {
+		nums := re.FindAllString(position, -1)
+		x, _ := strconv.Atoi(nums[0])
+		y, _ := strconv.Atoi(nums[1])
+		z, _ := strconv.Atoi(nums[2])
+
+		moon := &Moon{
+			Position: Axis{x, y, z},
+		}
+		moons = append(moons, moon)
+	}
+
+	for k := 0; k < simulations; k++ {
+		for i, moon := range moons {
+			for j := i + 1; j < len(moons); j++ {
+				moon.applyGravity(moons[j])
+			}
+		}
+
+		for _, moon := range moons {
+			moon.applyVelocity()
+		}
+	}
+
+	total := 0
+	for _, moon := range moons {
+		total += moon.getTotalEnergy()
+	}
+
+	return total
+}
+
+func Abs(n int) int {
+	if n < 0 {
+		return -n
+	}
+	return n
 }
--- 12_the_n_body_problem/12_the_n_body_problem_test.go
+++ 12_the_n_body_problem/12_the_n_body_problem_test.go
@@ -1,20 +1,46 @@
 package twelve_the_n_body_problem
 
 import (
+	"bufio"
+	"os"
 	"testing"
 )
 
 func TestTheNBodyProblemPartOneExampleInput(t *testing.T) {
-	exampleInput := []string{
-		"<x=-1, y=0, z=2>",
-		"<x=2, y=-10, z=-7>",
-		"<x=4, y=-8, z=8>",
-		"<x=3, y=5, z=-1>",
+	exampleInputs := [][]string{
+		{
+			"<x=-1, y=0, z=2>",
+			"<x=2, y=-10, z=-7>",
+			"<x=4, y=-8, z=8>",
+			"<x=3, y=5, z=-1>",
+		},
+		{
+			"<x=-8, y=-10, z=0>",
+			"<x=5, y=5, z=10>",
+			"<x=2, y=-7, z=3>",
+			"<x=9, y=-8, z=-3>",
+		},
 	}
-	expectedOutput := 179
+	exampleSimulations := []int{10, 100}
+	expectedOutputs := []int{179, 1940}
 
-	output := TheNBodyProblemPartOne(exampleInput)
-	if output != expectedOutput {
-		t.Fail()
+	for i, input := range exampleInputs {
+		output := TheNBodyProblemPartOne(input, exampleSimulations[i])
+		if output != expectedOutputs[i] {
+			t.Fail()
+		}
 	}
+}
+
+func TestTheNBodyProblemPartOneActualInput(t *testing.T) {
+	var input []string
+	file, _ := os.Open("./12_input.txt")
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		line := scanner.Text()
+		input = append(input, line)
+	}
+
+	output := TheNBodyProblemPartOne(input, 1000)
+	t.Log(output)
 }
