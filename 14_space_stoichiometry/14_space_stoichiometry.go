18f9b95f25f259e2bb1a25c4d3f4cc61e0dbd527
Bernd Hartzer <berndhartzer@gmail.com>
2019-12-19 20:14:12 +1000

    day 14 incomplete

--- 14_space_stoichiometry/14_space_stoichiometry.go
+++ 14_space_stoichiometry/14_space_stoichiometry.go
@@ -1,5 +1,16 @@
 package fourteen_space_stoichiometry
 
+import (
+	"fmt"
+	"strings"
+)
+
 func SpaceStoichiometryPartOne(reactions []string) int {
+	for _, reaction := range reactions {
+		split := strings.Split(reaction, "=>")
+		fmt.Println(split)
+
+	}
+
 	return 0
 }
